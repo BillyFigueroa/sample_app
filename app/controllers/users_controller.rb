887aa05868e54569483d079e58cb04e1d9dcaa83
Billy Figueroa <figuer25@gmail.com>
2013-07-28 19:06:29 -0400

    Adding minimal validation as well as error handling and display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params[:user])
+
+    if @user.save
+        redirect_to @user
+    else
+        render 'new'
+    end
 
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+    attr_accessible :email, :name
+
+    validates :name,
+        presence:   true,
+        length:     { minimum: 6, maximum: 20 }
+
+    validates :email,
+        presence: true
 end
