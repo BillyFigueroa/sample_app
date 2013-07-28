bc9cbc4561933c9a030c97b3e60231c42684d704
Billy Figueroa <figuer25@gmail.com>
2013-07-28 13:24:20 -0400

    Importing bootstrap. Adding actions. Displaying all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
+    @user = User.new
   end
 
   def edit
+    
   end
 
   def show
+
   end
 
-  def index
+  def create
+
   end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
